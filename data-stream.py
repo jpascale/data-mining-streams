bfeb2aba26920c919b483e1f4f913707cf53fd4f
jpascale <jpascale@itba.edu.ar>
2016-12-04 10:44:03 +0100

    Add update method and dependants

--- data-stream.py
+++ data-stream.py
@@ -6,8 +6,21 @@ class Edge(object):
 		self.x = x
 		self.y = y
 
+	def __eq__(self, other):
+		"""Override the default Equals behavior"""
+		if isinstance(other, self.__class__):
+			return self.x == other.x and self.y == other.y
+		return False
+
+	def __ne__(self, other):
+		"""Define a non-equality test"""
+		return not self.__eq__(other)
+
 	def __str__(self):
 		return "(" + str(self.x) + "," + str(self.y) + ")"
+
+	def forms_wedge(self, other):
+		return self.y == other.x or other.y == self.x
 
 	__repr__ = __str__
 
@@ -16,6 +29,9 @@ class Wedge(object):
 		self.first = first
 		self.second = second
 
+	def is_closed_by(self, et):
+		return self.second.y == et.x and et.y == self.first.x
+		## or self.first.x == et.x and self.second.y == et.y
 
 class StreamReader(object):
 
@@ -30,7 +46,7 @@ class StreamReader(object):
 		self.edge_res = [None for x in range(self.se)]
 		self.wedge_res = [None for x in range(self.sw)]
 
-		self.Nt = list()
+		#self.Nt = list()
 		self.tot_wedges = 0
 		self.is_closed = [False for x in range(self.sw)]
 
@@ -55,6 +71,12 @@ class StreamReader(object):
 
 	def update(self, et, t):
 		
+		for i in range(self.sw):
+			if self.wedge_res[i] is not None:
+				if self.wedge_res[i].is_closed_by(et):
+					self.is_closed[i] = True
+
+		#################### reservoir edge
 		change = False
 
 		for i in range(self.se):
@@ -62,6 +84,38 @@ class StreamReader(object):
 			if x <= 1.0/t:
 				self.edge_res[i] = et
 				change = True
+		#################### /reservoir edge
+
+		if change:
+			self.update_total_wedges()
+			new_wedges, nt = self.determine_new_wedges(et)
+
+			if new_wedges > 0: #Added by me, if not could divide by zero or enter inexistent array index
+				for i in range(self.sw):
+					x = random.uniform(0.0, 1.0)
+					if x <= float(new_wedges) / float(self.tot_wedges):
+						urand = random.randint(0, len(nt)-1)
+						self.wedge_res[i] = nt[urand]
+						self.is_closed[i] = False
+
+	def update_total_wedges(self):
+		tot_wedges = 0
+		for i in range(self.se):
+			for j in range(self.se):
+				if i < j:
+					if self.edge_res[i].forms_wedge(self.edge_res[j]):
+						tot_wedges += 1
+		self.tot_wedges = tot_wedges
+
+	#new wedges are the ones who involve et
+	def determine_new_wedges(self, et):
+		nt = []
+		for i in range(self.se):
+			if et.y == self.edge_res[i].x:
+				nt.append(Wedge(et, self.edge_res[i]))
+			elif self.edge_res[i].y == et.x:
+				nt.append(Wedge(self.edge_res[i], et))
+		return len(nt), nt
 
 if __name__ == '__main__':
 	StreamReader("data.dat", 50, 50).start_stream()
